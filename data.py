bf999f9b7058080137fc49ac5593f380593556a4
Abhaybhadouriya <50852249+Abhaybhadouriya@users.noreply.github.com>
2020-10-02 09:17:03 +0530

    Update data.py

--- data.py
+++ data.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup as bs
 
 def getdata(value,lang):
 	try:
-		
+		if(value=='all'):
+                     r=requests.get("https://inshorts.com/"+lang+"/read").text
+                else:		
 		r=requests.get("https://inshorts.com/"+lang+"/read/"+value).text
 		soup=bs(r,'lxml')
 		x=soup.find_all('div',class_="news-card z-depth-1")
